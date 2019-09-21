c1bdfbf7d711cb83a1be01a26320739e79ff9dbd
jorgeogj@gmail.com <dxn-Gswx4x3Q5K>
2019-09-20 19:31:33 -0500

    Agregue la carpeta dist al gitignore y un formato de estilo a los archivos

--- src/app/_services/lab/solicitud.service.ts
+++ src/app/_services/lab/solicitud.service.ts
@@ -13,54 +13,54 @@ export class SolicitudService {
     public identity: string;
     public token: string;
 
-    constructor( private _http: Http) {
+    constructor(private _http: Http) {
         this.url = Cfg.BackendUrl;
     }
 
-    solicitud_test () {
+    solicitud_test() {
         return "solicitud de servicio ";
     }
 
 
-	solicitud_list(){               
-        let headers = new Headers({'Content-Type':'application/json'});
-        return this._http.get(this.url+'/solicitud', {headers: headers}).map(
+    solicitud_list() {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        return this._http.get(this.url + '/solicitud', { headers: headers }).map(
             res => res.json()
-            ); 
-    }
- 
-    solicitud_uno(_id){  
-        let mkid=_id; 
-        let headers = new Headers({'Content-Type':'application/json'});
-        return this._http.get(this.url+'/solicitud/'+mkid, {headers:headers}).map(
-            res=>res.json()        
-            );
-   }   
-
-   solicitud_unod(_id){  
-    let mkid=_id; 
-    let headers = new Headers({'Content-Type':'application/json'});
-    return this._http.get(this.url+'/solicitudD/'+mkid, {headers:headers}).map(
-        res=>res.json()        
-        );
-} 
-
-   solicitud_new(paciente){
-    let params = JSON.stringify(paciente); 
-    let headers = new Headers({
-          'Content-Type': 'application/json'
-    });
-    return this._http.post(this.url+'/solicitud/new',params,{headers:headers}).map(
-        res=>res.json()
         );
     }
 
-    solicitud_edit(paciente,_id){   
-        let mkid = _id;  
+    solicitud_uno(_id) {
+        let mkid = _id;
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        return this._http.get(this.url + '/solicitud/' + mkid, { headers: headers }).map(
+            res => res.json()
+        );
+    }
+
+    solicitud_unod(_id) {
+        let mkid = _id;
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        return this._http.get(this.url + '/solicitudD/' + mkid, { headers: headers }).map(
+            res => res.json()
+        );
+    }
+
+    solicitud_new(paciente) {
         let params = JSON.stringify(paciente);
-        let headers = new Headers({'Content-Type':'application/json'});
-        return this._http.put(this.url + '/solicitud/' + mkid, params, {headers: headers}).map(
+        let headers = new Headers({
+            'Content-Type': 'application/json'
+        });
+        return this._http.post(this.url + '/solicitud/new', params, { headers: headers }).map(
             res => res.json()
-            );
-    }    
+        );
+    }
+
+    solicitud_edit(paciente, _id) {
+        let mkid = _id;
+        let params = JSON.stringify(paciente);
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        return this._http.put(this.url + '/solicitud/' + mkid, params, { headers: headers }).map(
+            res => res.json()
+        );
+    }
 }
